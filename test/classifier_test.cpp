ebf225c5444f1a5b734758d8964fbf407c429377
Danielle Yang <dy6@illinois.edu>
2019-03-22 12:33:50 -0500

    Wrote tests for GetPosteriorProb methods

--- test/classifier_test.cpp
+++ test/classifier_test.cpp
@@ -17,11 +17,35 @@ void Initialize() {
 
 // ------------------------------- GET POSTERIOR PROBABILITY TESTS ------------------------------------
 
-TEST_CASE("Tests GetPosterior Probability: ") {
+TEST_CASE("Tests GetPosteriorProb: Image = 0, Digit = 0") {
     classifier.SetUp();
     vector<vector<char>> image = classifier.CreateImageVector(28);
     double prob = classifier.GetPosteriorProb(image, 0);
-    REQUIRE(0.4 == prob);
+    REQUIRE(AreEquivalent(-78.6579, prob));
+}
+
+TEST_CASE("Tests GetPosteriorProb: Image = 0, Digit = 3") {
+    classifier.SetUp();
+    vector<vector<char>> image = classifier.CreateImageVector(28);
+    double prob = classifier.GetPosteriorProb(image, 3);
+    REQUIRE(AreEquivalent(-110.002300991, prob));
+}
+
+// ------------------------------- GET ALL POSTERIOR PROBABILITIES TESTS ------------------------------------
+
+TEST_CASE("Tests GetAllPosteriorProbs: Image = 0") {
+    classifier.SetUp();
+    vector<vector<char>> image = classifier.CreateImageVector(28);
+    vector<double> probs = classifier.GetAllPosteriorProbs(image);
+    REQUIRE(AreEquivalent(-78.6579, probs[0]));
+    REQUIRE(AreEquivalent(-105.106, probs[8]));
+}
+
+TEST_CASE("Tests GetAllPosteriorProbs: Image = 9") {
+    classifier.SetUp();
+    vector<vector<char>> image = classifier.CreateImageVector(0);
+    vector<double> probs = classifier.GetAllPosteriorProbs(image);
+    REQUIRE(AreEquivalent(-70.6831, probs[9]));
 }
 
 // ------------------------------- CLASSIFIER TEST ----------------------------------------------------
